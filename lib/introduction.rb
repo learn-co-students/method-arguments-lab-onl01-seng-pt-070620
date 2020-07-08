00ac80ede0317963e4fe69c58fcd39d8287bc793
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-08 04:07:55 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction ("Josh")
+
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction ("Dan", "Ember.js")
